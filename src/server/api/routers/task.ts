01960c39cf973641fee54b32a5456ba9a846eb36
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-09-10 19:44:43 -0300

    feat: add subtask router

---
+++ src/server/api/routers/task.ts
@@ -0,0 +1,38 @@
+import {  PrismaClient } from "@prisma/client/extension";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+
+const idSchema = z.object({
+  id: z.number()
+})
+
+const taskSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+})
+
+export const tasksRouter = createTRPCRouter({
+  get: publicProcedure.query(async ({ ctx }) => {
+    const tasks = await ctx.db.task.findMany()
+    return tasks ?? null;
+  }),
+  create: publicProcedure
+    .input(taskSchema)
+    .mutation(async ({ ctx, input}) => {
+      return ctx.db.task.create({
+        data: {
+            title: input.title,
+            description: input.description
+        }
+      })
+    }),
+    delete: publicProcedure
+    .input(idSchema)
+    .mutation(async ({ ctx, input}) => {
+      await ctx.db.task.delete({
+        where: {
+          id: input.id
+        }
+      })
+    })
+})
